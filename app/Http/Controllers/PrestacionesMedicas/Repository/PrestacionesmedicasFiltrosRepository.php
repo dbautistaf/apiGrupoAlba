80ad3ea4444d962b93d1b49a79c35d639a2627ed
Danny Bautista <capoajedrez.29@gmail.com>
2025-10-19 19:11:52 -0500

    One

--- app/Http/Controllers/PrestacionesMedicas/Repository/PrestacionesmedicasFiltrosRepository.php
+++ app/Http/Controllers/PrestacionesMedicas/Repository/PrestacionesmedicasFiltrosRepository.php
@@ -93,7 +93,14 @@ class PrestacionesmedicasFiltrosRepository
         if (!is_null($tramite)) {
             $query->where('numero_tramite', 'like', $tramite . '%');
         }
-        $results = $query->orderBy('fecha_registra', 'desc')
+        $results = $query
+            ->orderByRaw("
+                CASE 
+                    WHEN fecha_modifica IS NULL OR fecha_modifica = '' 
+                    THEN fecha_registra 
+                    ELSE fecha_modifica 
+                    END DESC
+                ")
             ->orderBy('cod_prestacion', 'desc')
             ->limit($limit)
             ->get();
@@ -119,7 +126,14 @@ class PrestacionesmedicasFiltrosRepository
 
     public function findByDeleteId($id)
     {
-        return PrestacionesPracticaLaboratorioEntity::find($id)->delete();
+        //return PrestacionesPracticaLaboratorioEntity::find($id)->delete();
+
+        $registro = PrestacionesPracticaLaboratorioEntity::find($id);
+
+        if ($registro) {
+            $registro->cod_tipo_estado = 7;
+            $registro->save();
+        }
     }
 
     public function findByListDniAfiliado($dni)
